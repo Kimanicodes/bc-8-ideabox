08d79fc6dfc7fdc5700dcf999c9cb3d04cc2b84a
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-06-21 10:45:04 +0300

    First commit with the initial test data for the local environment

---
+++ manage.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+'''Creating an instance of the flask class'''
+ideabox_app = Flask (__name__)
+
+@ideabox_app.route('/')
+def hello():
+	return 'Hello IdeaBox!'
+
+@ideabox_app.route('/Home')
+def hello_home():
+	return 'Hello Home'
+
+@ideabox_app.route('/About')
+def hello_about():
+	return 'Hello About'
+
+if __name__ == "__main__":
+	ideabox_app.run(debug = True)

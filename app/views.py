64a6686e9b3b8763f5b1c8561cb0d0ec53793e48
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-06-21 18:47:03 +0300

    Added changes to views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
-# from flask import render_template
-# from manage import ideabox_app
+from flask import render_template
+from manage import db
+from forms import ShowSignUp
+
+@ideabox_app.route('/')
+def view_homepage():
+    return render_template('index.html')
 
 
-# @ideabox_app.route('/')
-# def view_homepage():
-#     return render_template('index.html')
+#@ideabox_app.route('/showSignUp', methods=['GET', 'POST'])
+#def register():
+#    form = ShowSignUp(request.form)
+#    if request.method == 'POST' and form.validate():
+#        user = User(form.username.data, form.first_name.data,form.last_name.data,form.email.data,
+#                    form.password.data)
+#        db_session.add(user)
+#        flash('Thank You for registering!')
+#        return redirect(url_for('login'))
+#    return render_template('register.html', form=form)
